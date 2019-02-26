4271c42ddb29e96fca0108cf2dac11b053652e19
Courtney Ott <courtneyott@Courtneys-MacBook-Pro-2.local>
2019-02-26 16:46:19 -0700

    Setup production vs dev Cors handling

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -21,7 +20,18 @@ db.once('open', onDBConnected)
 function onDBConnected(){
   console.log('we are connected to mongo db')
 }
-app.use(cors());
+
+if(process.env.NODE_ENV === "production"){
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://fathomless-ocean-75062.herokuapp.com/');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  })
+} else {
+  const cors = require('cors')
+  app.use(cors());
+}
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', getSpeech );
 app.post('/', createSpeech);
